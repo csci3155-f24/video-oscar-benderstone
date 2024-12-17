feb517fb4ff75f9ee24509a38952f09f7c4021a4
Oscar Bender-Stone <oscarbenderstone@gmail.com>
2024-12-17 16:06:59 -0700

    Finished script and added missing TypeScript files

--- README.md
+++ README.md
@@ -8,22 +8,35 @@ See [instructions.md](instructions.md) for submission instructions.
 
 ## Description
 
-TOD: Replace this with at least a few paragraphs describing your project.
+Express is a popular JavaScript web framework. It features an easy-to-use API for working with HTTP requests and routes. The framework is light-weight, well-maintained, and is customizable via user-made middleware. Express is widely used, being downloaded 32,820,381 times weekly and being used in 87,467 other npm packages [2]. An extension, via the DefinitelyTyped library, adds detailed type annotations, making Express.JS easier to use in TypeScript.
 
-Electron is a framework used to create. Electron features many 
+Among these features, ExpressJS exemlifies many PPL concepts. It has a strong focus on functions, treating functions as values, using higher-order functions, and including callbacks. It also has a strong emphasis on data types, with abstract data types for requests and responses and parametric types via DefinitelyTyped.
 
 
-Electron features many .
-
-To display Electron's features, my video outlines the key properties.
+To demo these concepts in Express, this project uses a TypeScript file with several examples. Each example has its own webpage displaying the corresponding output. However, the concepts are best displayed in the code itself, found in `src/index.ts`.
 
 ## Repository Organization
-
-TODO: Replace this with a description of the organization of your repository.
+- `src/index.ts`: contains the main source file used in the demo, with type annotations.
+- `build/index.js`: contains the code that docker runs in the browser.
+- `README.md`
+- `script.md`
 
 ## Building and Testing Instructions
 
-TODO: Replace with building and testing instructions. Please minimize the number of dependencies and as simple and platform-independent as possible. Consider, for example, using [Docker](https://www.docker.com/).
+First, set up the project with npm: `npm install`.
+
+Then, run with `docker-compose`, with root privileges on Linux/MacOS: `sudo docker-compose up`.
+
+## Sources
+
+[1] "DefinitelyTyped/DefinitelyTyped", hosted on GitHub, [https://github.com/DefinitelyTyped/DefinitelyTyped].
+
+[2] "express", NPM, [https://www.npmjs.com/package/express].
+
+[3] "expressjs/express", hosted on GitHub, [https://github.com/expressjs/express].
+
+[4] OpenJS Foundation, "Express", [https://expressjs.com].
+
 
 ## Presentation
 
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,100 @@ import express, {Request, Response} from 'express';
 
 const app = express();
 const port = 3000;
+const router = express.Router()
+
+// Concept 1: Functions 
+//
+// Adapted (with extra comments) from:
+// Express Documentation
+// https://expressjs.com/en/guide/using-middleware.html
+//
+// From the Documentation:
+// "Middleware functions can perform the following tasks:
+
+// - Execute any code.
+// - Make changes to the request and the response objects.
+// - End the request-response cycle.
+// - Call the next middleware function in the stack.""
+
+// Functions as Values
+// Note: next uses any types for parameters and returned expression
+function logTime(req: Request, res: Response, next: Function) {
+  console.log('Time:', Date.now())
+  next()
+}
+
+//a middleware function with no mount path. This code is executed for every request to the router
+router.use(logTime)
+
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+function getRequestURL(req: Request, res: Response, next: Function) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}
+
+function getRequestType(req: Request, res: Response, next: Function) {
+  console.log('Request Type:', req.method)
+  next()
+}
+
+router.use('/user/:id', getRequestURL, getRequestType)
+
+// TYPE: (string, (Request, Response, next: (...args: any) => any))
+
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', (req: Request, res: Response, next) => {
+  // if the user ID is 0, skip to the next router
+  console.log(req.params.id)
+  if (req.params.id === '1') next('route')
+  // otherwise pass control to the next middleware function in this stack
+  else next()
+}, (req: Request, res: Response, next) => {
+  // render a regular page
+  res.send('USR: regular')
+})
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', (req, res, next) => {
+  console.log(req.params.id)
+  res.send('USER: special')
+})
+
+// Alternatively, these can be chained:
+router
+    .get('/user1/:id', (req: Request, res: Response, next) => {
+  // if the user ID is 0, skip to the next router
+  console.log(req.params.id)
+  if (req.params.id === '1') next('route')
+  // otherwise pass control to the next middleware function in this stack
+  else next()
+}, (req: Request, res: Response, next) => {
+  // render a regular page
+  res.send('USR: regular')
+})
+    .get('/user1/:id', (req, res, next) => {
+  console.log(req.params.id)
+  res.send('USER: special')
+})
+
+// mount the router on the app
+app.use('/', router)
+
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+    res.redirect('/user/:id')
+})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// END Example
 
-
-// Adapted from: 
+// Concept 2: Data Types
+//
+// See link:
 // DefinitelyTyped/DefinitelyTyped/types/express
 // License: MIT license
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/3c2ad7892a2d12f005177087a909635a71a50bcf/types/express/index.d.ts#L96
-app.get('/types-express', (req: Request, res: Response) => {
 
-
-})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
